c0cfb9b22f5835c857fa7ccf00b5bd709171f1f9
AlexandraAlter <alexandra.alter@protonmail.com>
2021-05-30 00:05:07 +0100

    Clarified names and reworked logic until it finally works - mostly as expected

--- plover_geminipr_footpedal/machine.py
+++ plover_geminipr_footpedal/machine.py
@@ -24,32 +24,36 @@ class GeminiPrFootpedal(GeminiPr):
 
     def __init__(self, params):
         super().__init__(params)
+
         self._footpedal_capture = None
         self._is_suppressed = False
-        # Currently held keys.
-        self._down_keys = set()
-        # All keys part of the stroke.
-        self._stroke_keys = set()
-        self._last_stroke_key_down_count = 0
-        self._stroke_key_down_count = 0
-        self._update_bindings()
+        self._footpedal_keys_down = set()  # Currently held keys.
+        self._footpedal_keys = set()  # All keys part of the stroke.
+        self._footpedal_key_down_count = 0
+        self._ignore_footpedal_stroke = False
 
-    def _suppress(self):
+        self._update_footpedal_bindings()
+
+    def _suppress_footpedal(self):
         if self._footpedal_capture is None:
             return
-        suppressed_keys = GeminiPrFootpedal.FOOTPEDAL_KEYS if self._is_suppressed else ()
+        footpedal_keys = self._footpedal_bindings.keys()
+        suppressed_keys = footpedal_keys if self._is_suppressed else ()
         self._footpedal_capture.suppress_keyboard(suppressed_keys)
 
-    def _update_bindings(self):
-        self._bindings = dict(self.keymap.get_bindings())
-        for key, mapping in list(self._bindings.items()):
+    def _update_footpedal_bindings(self):
+        self._footpedal_bindings = dict(self.keymap.get_bindings())
+        for key, mapping in list(self._footpedal_bindings.items()):
+            if key not in GeminiPrFootpedal.FOOTPEDAL_KEYS:
+                del self._footpedal_bindings[key]
+                continue
             if 'no-op' == mapping:
-                self._bindings[key] = None
-        self._suppress()
+                self._footpedal_bindings[key] = None
+        self._suppress_footpedal()
 
     def set_keymap(self, keymap):
         super().set_keymap(keymap)
-        self._update_bindings()
+        self._update_footpedal_bindings()
 
     def start_capture(self):
         try:
@@ -60,62 +64,50 @@ class GeminiPrFootpedal(GeminiPr):
         except:
             self._error()
             raise
-
         super().start_capture()
 
     def stop_capture(self):
         if self._footpedal_capture is not None:
             self._is_suppressed = False
-            self._suppress()
+            self._suppress_footpedal()
             self._footpedal_capture.cancel()
             self._footpedal_capture = None
-
         super().stop_capture()
 
     def set_suppression(self, enabled):
         self._is_suppressed = enabled
-        self._suppress()
+        self._suppress_footpedal()
 
     def _footkey_down(self, key):
         """Called when a key is pressed."""
         assert key is not None
-        self._stroke_key_down_count += 1
-        self._down_keys.add(key)
-        self._stroke_keys.add(key)
+        self._footpedal_key_down_count += 1
+        self._footpedal_keys_down.add(key)
+        self._footpedal_keys.add(key)
 
     def _footkey_up(self, key):
         """Called when a key is released."""
         assert key is not None
-        self._down_keys.discard(key)
+        self._footpedal_keys_down.discard(key)
         # A stroke is complete if all pressed keys have been released.
-        if self._down_keys or not self._stroke_keys:
+        if self._footpedal_keys_down or not self._footpedal_keys:
             return
-        self._last_stroke_key_down_count = self._stroke_key_down_count
-        steno_keys = {self._bindings.get(k) for k in self._stroke_keys}
+        steno_keys = {
+            self._footpedal_bindings.get(k)
+            for k in self._footpedal_keys
+        }
         steno_keys -= {None}
-        if steno_keys:
+        if steno_keys and not self._ignore_footpedal_stroke:
             self._notify(steno_keys)
-        self._stroke_keys.clear()
-        self._stroke_key_down_count = 0
+        self._footpedal_keys.clear()
+        self._footpedal_key_down_count = 0
+        self._ignore_footpedal_stroke = False
 
     def _notify(self, steno_keys):
-
+        if self._footpedal_keys_down:
+            steno_keys += {
+                self._footpedal_bindings.get(k)
+                for k in self._footpedal_keys_down
+            }
+            self._ignore_footpedal_stroke = True
         super()._notify(steno_keys)
-
-    def run(self):
-        """Overrides base class run method. Do not call directly."""
-        self._ready()
-        for packet in self._iter_packets(BYTES_PER_STROKE):
-            if not (packet[0] & 0x80) or sum(b & 0x80 for b in packet[1:]):
-                log.error('discarding invalid packet: %s',
-                          binascii.hexlify(packet))
-                continue
-            steno_keys = []
-            for i, b in enumerate(packet):
-                for j in range(1, 8):
-                    if (b & (0x80 >> j)):
-                        steno_keys.append(STENO_KEY_CHART[i * 7 + j - 1])
-            steno_keys = self.keymap.keys_to_actions(steno_keys)
-            if steno_keys:
-                self._notify(steno_keys)
-
